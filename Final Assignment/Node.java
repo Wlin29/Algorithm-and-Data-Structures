c8b3cf9f77dbfd6cd14a409b9e1ebd42cdf4c14a
Wlin29 <73469932+Wlin29@users.noreply.github.com>
2022-04-08 23:36:25 +0100

    Added Node.java
    Node.java supplies the nodes required for the ternary search tree

---
+++ Final Assignment/Node.java
@@ -0,0 +1,20 @@
+/*
+    Nodes of the ternary search tree
+ */
+
+public class Node {
+
+    char data;
+    Node left;
+    Node right;
+    Node middle;
+    String[] stopInfo = new String[10];
+
+    public Node(char data, String[]stopInfo){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.middle = null;
+        this.stopInfo = null;
+    }
+}
